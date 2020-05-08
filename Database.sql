0505c76ac12314ba86d2356a6876304b28b6c298
nnafzaoui <nafzaouinourdine@gmail.com>
2020-05-08 03:22:44 +0000

    order of instructions changed

--- Database.sql
+++ Database.sql
@@ -9,51 +9,6 @@ GRANT all privileges on * . * TO 'nourdine1'@'localhost';
 CREATE USER 'U2'@'localhost' IDENTIFIED BY 'pass2';
 GRANT INSERT, DELETE ON animals.* TO 'U2'@'localhost';
 
-/**/
-
-insert into Animal values(2, 3, 'male');
-insert into Animal values(3, 6, 'female');
-insert into Animal values(4, 9, 'female');
-
-/**/
-
-insert into Duck values(2, 'Yellow');
-insert into Fish values(3, 20, true);
-insert into Zebra values(4, true);
-
-/**/
-
-update Animal 
-set age = 33
-where id = 1;
-
-update Duck
-set beakColor = 'red'
-where id = 2;
-
-update Fish
-set sizeInCm = 100
-where id = 3;
-
-update Zebra
-set is_wild = false
-where id = 4;
-
-
-/*  */
-
-Delete from Zebra where id = 4;
-delete from Fish where id = 3;
-delete from Duck where id = 2;
-delete from Animal where id = 1;
-
-/*  */
-
-select * from Animal;
-select * from Duck;
-select * from Fish;
-select * from Zebra;
-
 
 /*==============================================================*/
 /* Table : Animal                                               */
@@ -105,3 +60,51 @@ alter table Fish add constraint FK_Generalisation_2 foreign key (id)
 
 alter table Zebra add constraint FK_Generalisation_3 foreign key (id)
       references Animal (id) on delete restrict on update restrict;
+      
+      
+/**/
+
+insert into Animal values(2, 3, 'male');
+insert into Animal values(3, 6, 'female');
+insert into Animal values(4, 9, 'female');
+
+/**/
+
+insert into Duck values(2, 'Yellow');
+insert into Fish values(3, 20, true);
+insert into Zebra values(4, true);
+
+/**/
+
+update Animal 
+set age = 33
+where id = 1;
+
+update Duck
+set beakColor = 'red'
+where id = 2;
+
+update Fish
+set sizeInCm = 100
+where id = 3;
+
+update Zebra
+set is_wild = false
+where id = 4;
+
+
+/*  */
+
+Delete from Zebra where id = 4;
+delete from Fish where id = 3;
+delete from Duck where id = 2;
+delete from Animal where id = 1;
+
+/*  */
+
+select * from Animal;
+select * from Duck;
+select * from Fish;
+select * from Zebra;
+
+
